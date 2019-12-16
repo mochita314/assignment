af8391c9d0ed499bb8b82985f77e1d9e27a81bc0
鬼澤英里 <kizawaeri@kizawaerinoMacBook-Pro.local>
2019-12-16 20:12:47 +0900

    1216

--- nn.py
+++ nn.py
@@ -1,4 +1,66 @@
-import numpy
+#!/usr/bin/python
+
+import numpy as np
 import math
 import random
+import argparse
 
+# build a neural network
+
+class Sigmoid:
+
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.param = False
+    
+    # forward propagation
+    def __call__(self,x):
+        self.x = x
+        y = 1 / (1 + np.exp(-self.x))
+        self.y = y
+        return y
+    
+    # backword propagation
+    def backward(self,dout):
+        return dout*self.y*(1-self.y)
+
+class Relu:
+
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.param = False
+    
+    # forward propagation
+    def __call__(self,x):
+        self.x = x
+        y = x*(x>0)
+        self.y = y
+        return y
+    
+    # backward propagation
+    def backward(self,dout):
+        return dout*(self.x>0)
+
+class Softmax:
+
+    def __init__(self):
+        self.x = None
+        self.y = None
+        self.param = False
+    
+    # forward propagation
+    def __call__(self,x):
+        self.x = x
+        exp_x = np.exp(x-x.max(axis=1,keepdims=True)) # prevent overflow
+        sum_exp_x = np.sum(exp_x,axis=1,keepdims=True)
+        y = exp_x / sum_exp_x
+        self.y = y
+        return y
+
+    
+        
+
+
+        
---
+++ train.py
@@ -0,0 +1,9 @@
+
+
+parser = argparse.ArgumentParser(description='Train neural network')
+parser.add_argument('--batchsize', '-b', type=int, default=16, help='Number of images in each mini-batch')
+parser.add_argument('--iteration', '-i', type=int, default=100, help='Number of iteration times')
+parser.add_argument('--epoch', '-e', type=int, default=1, help='Number of epoch times')
+parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
+args = parser.parse_args()
+
