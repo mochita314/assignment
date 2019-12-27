9ef85f4f95dda41cb234da8d5dd5e3c627e3045d
鬼澤英里 <kizawa-eri@g.ecc.u-tokyo.ac.jp>
2019-12-27 12:26:32 +0900

    1227

---
+++ preprocess.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+# -*- データの前処理を行う関数を定義したファイル -*-
+
+import urllib.request
+import numpy as np
+import gzip
+import pickle
+import random
+
+def load_image_and_label(data_file_name,label_file_name):
+
+    dic = {}
+    
+    dataset_dir = 'data'
+
+    file_path = dataset_dir + '/' + data_file_name
+    with gzip.open(file_path,'rb') as f:
+        data = np.frombuffer(f.read(),np.uint8,offset=16)
+    data = data.reshape(-1,784)
+    dic['data'] = data
+    
+    file_path = dataset_dir + '/' + label_file_name
+    with gzip.open(file_path,'rb') as f:
+        labels = np.frombuffer(f.read(),np.uint8,offset=8)
+    dic['labels'] = labels
+
+    return dic
+
+def noise(arr,d):
+
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            p = random.randint(1,100)
+            if p>=d:
+                q = random.randint(0,255)
+                arr[i][j] = q
+
+    return arr
+
+def normalize(arr):
+
+    arr = arr.astype(np.float32)
+    arr /= 255
+
+    return arr
+
+def one_hot(label):
+
+    one_hot_label = np.zeros((label.size,10))
+    for i in range((label.size)):
+        one_hot_label[i][label[i]] = 1
+
+    return one_hot_label
+
--- train.py
+++ train.py
@@ -1,49 +1,76 @@
 #!/usr/bin/python
 
+import argparse
+
 import numpy as np
 import math
 import random
 
+import urllib.request
+import glob
+
+import gzip
+import pickle
+
 from nn import *
+from preprocess import *
 
 def train(model,optimizer,epoch,batchsize):
 
-    train_loss_lst = []
-    train_acuracy_lst = []
-    test_loss_lst = []
-    test_acuracy_lst = []
-
-    epo = 0
-
-    while epo < epoch:
-
-        # initialize loss
+    for epo in range(epoch):
         sum_loss = 0
+        pred_y = []
 
+        # 訓練データをランダムに並び替える
+        random_sort = np.random.permulation(train_data)
+
+        for i in range(0,len(train_data),batchsize):
+            x = train_data[random_sort[i:i+batchsize]]
+            t = train_label[random_sort[i:i+batchsize]]
+
+            loss = model.forward(x,t)
+            model.backward()
+            optimizer.update()
+
+            sum_loss += loss
+
+            pred = np.argmax(model.y,axis=1).tolist()
+            pred_y.extend(pred)
         
+        train_loss = sum_loss / len(train_data)
+        train_accuracy = np.sum(one_hot(pred)*train_label[random_sort])/len(train_data)
 
+        sum_loss = 0
+        pred_y = []
+        for i in range(0, len(test_data), batchsize):
+            x = test_x[i: i+batchsize]
+            t = test_y[i: i+batchsize]
 
+            sum_loss += model.forward(x, t, train_config=False)
+            pred = np.argmax(model.y, axis=1).tolist()
+            pred_y.extend(pred)
+        
+        test_loss = sum_loss / len(test_data)
+        test_accuracy = np.sum(one_hot(pred)*test_label[random_sort])/len(test_data)
 
-
-
-        epo+=1
-    
-
-    return train_loss_lst,train_acuracy_lst,test_loss_lst,test_acuracy_lst
+        print('train | loss {:.4f},accuracy{:.4f}'.format(float(train_loss),train_accuracy))
+        print('test | loss {:.4f},accuracy{:.4f}'.format(float(test_loss),test_accuracy))
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Train neural network')
-    parser.add_argument('--batchsize', '-b', type=int, default=16, help='Number of images in each mini-batch')
+    parser.add_argument('--batchsize', '-b', type=int, default=100, help='Number of images in each mini-batch')
     #parser.add_argument('--iteration', '-i', type=int, default=100, help='Number of iteration times')
-    parser.add_argument('--epoch', '-e', type=int, default=20, help='Number of epoch times')
-    parser.add_argument('--lr', type=float, default=0.5, help='Learning rate')
+    parser.add_argument('--epoch', '-e', type=int, default=14, help='Number of epoch times')
+    parser.add_argument('--lr', type=float, default=1.0, help='Learning rate')
     args = parser.parse_args()
 
-    # define optimizer
+    # -*- モデルの定義 -*-
+
+    # optimizerを定義
     optimizer = SGD(lr=args.lr)
 
-    # build a neural network
+    # ニューラルネットワークの構成を定義
     model = MLP()
     model.add_layer(Affine(784,1000))
     model.add_layer(ReLU())
@@ -53,4 +80,50 @@ if __name__ == '__main__':
 
     optimizer.setup(model)
 
-    train_loss_lst, train_acc_lst, test_loss_lst, test_acc_lst = train(model,optimizer,epoch=args.epoch,batchsize=args.batchsize)
+    # -*- MNISTのデータをzipファイルとして指定urlからダウンロードし前処理をする -*-
+
+    # データのあるURL
+    url = 'http://yann.lecun.com/exdb/mnist/'
+
+    # 保存先ディレクトリ
+    dataset_dir = 'data'
+
+    # ダウンロードするファイル
+    key_file = {
+    'train_img':'train-images-idx3-ubyte.gz',
+    'train_label':'train-labels-idx1-ubyte.gz',
+    'test_img':'t10k-images-idx3-ubyte.gz',
+    'test_label':'t10k-labels-idx1-ubyte.gz'
+    }
+
+    files = ['train-images-idx3-ubyte.gz','train-labels-idx1-ubyte.gz','t10k-images-idx3-ubyte.gz','t10k-labels-idx1-ubyte.gz']
+
+    if len(glob.glob('data/*'))!=0:
+        print("files are already downloaded")
+    else:
+        print("not yet")
+        for v in key_file.values():
+            file_path = dataset_dir + '/' + v
+            urllib.request.urlretrieve(url+v,file_path)
+
+    # numpyの配列に変換
+    train_set = load_image_and_label(files[0],files[1])
+    test_set = load_image_and_label(files[2],files[3])
+
+    # 学習データにノイズを加える
+    train_data = train_set['data']
+    print(type(train_data))
+
+    noise(train_data,0)
+
+    # データの正規化
+    train_data = normalize(train_data)
+    test_data = normalize(test_set['data'])
+
+    # ラベルをone-hot表現に変換
+    train_label = one_hot(train_set['labels'])
+    test_label = one_hot(test_set['labels'])
+
+    # 学習させる
+    #train(model,optimizer,epoch=args.epoch,batchsize=args.batchsize)
+
